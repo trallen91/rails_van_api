d1e19ae3db00dd5c769ce81b828e35c57d22c69f
Travis Allen <travis.william.allen@gmail.com>
2017-11-26 16:44:20 -0500

    Configure Twilio

--- van/app/controllers/rides_controller.rb
+++ van/app/controllers/rides_controller.rb
@@ -34,9 +34,32 @@ class RidesController < ApplicationController
     end
   end
 
+  def start_ride
+
+    send_text
+  end
+
+  def send_text
+    account_sid = "AC4def0e4247e3a242ad66b44c485c400a"
+    auth_token = "9b0d156bd4122f290f992a9603c652a6"
+    Twilio.configure do |config|
+      config.account_sid = account_sid
+      config.auth_token = auth_token
+    end
+
+    @client = Twilio::REST::Client.new
+
+    @client.messages.create({
+      from: "+17187362402",
+      to: "+13475998150",
+      body: "You driver is en route"
+    })
+    return nil
+  end
+
   def update_with_complete
      @ride = Ride.find_by(id: params[:id])
-     @ride.complete = true
+     @ride.status = "Complete"
      if @ride.save
        render :json => @ride.to_json
      else

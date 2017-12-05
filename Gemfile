c77c7eb1f5a4d858b164e74c765149cceaed1d05
Travis Allen <travis.william.allen@gmail.com>
2017-12-05 14:55:28 -0500

    get rid factory girl

--- Gemfile
+++ Gemfile
@@ -6,7 +6,6 @@ gem 'rails', '4.2.4'
 gem 'rails-api'
 gem 'rspec-core'
 gem 'rspec_junit_formatter'
-gem 'factory_girl_rails'
 
 gem 'spring', :group => :development
 
--- spec/models/driver_spec.rb
+++ spec/models/driver_spec.rb
@@ -6,8 +6,6 @@ class DriverTest < ActiveSupport::TestCase
   end
   RSpec.describe Driver, type: :model do
 
-    let(:driver) { FactoryGirl.create(:driver) }
-
     it "should validate the presence of driver name" do
       1 == 1
     end
